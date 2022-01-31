a3f2361eea949dcf5574cd765b417bd2b8e51df0
syokaturyou <teamsumagaku2@gmail.com>
2022-01-31 07:39:19 +0000

    1/31 1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,9 @@ class LessonsController < ApplicationController
     @hello = "こんにちは"
     @message = "helloアクションが実行されています"
   end
+  
+  def call
+    @call = "callアクションが実行されています"
+  end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get 'lessons/hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+   get 'lessons/hello', to: 'lessons#hello'
+   # 省略形 
+   # get 'lessons/hello'
+   
+   get 'lessons/call', to: 'lessons#call'
+  
 end
