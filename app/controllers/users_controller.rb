e83438de46e2b7eea26082b2da030b9dc09fda09
syokaturyou <teamsumagaku2@gmail.com>
2022-02-01 03:55:50 +0000

    2/1 1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  end
+# ここより下の各アクションを追加しましょう
+  def new 
+  end
+ 
+  def create
+  end
+ 
+  def show
+  end
+ 
+  def edit
+  end
+ 
+  def update
+  end
+ 
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   get 'lessons/hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
